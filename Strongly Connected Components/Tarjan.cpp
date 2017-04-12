02356d6a32ce456d3e135f1b98e575959f45de66
Coisin <odwyeroisin@gmail.com>
2017-04-12 20:09:42 +0100

    Fixed Tarjan, last commit was a mistake.

--- Strongly Connected Components/Tarjan.cpp
+++ Strongly Connected Components/Tarjan.cpp
@@ -9,67 +9,57 @@ Tarjan's Algorithm for finding Strongly Connected Components
 using namespace std;
 
 vector<vector<int> > adjList;
-vector<int> dfs_num, dfs_low, parent;
-stack<int> s;
+vector<int> dfs_num, dfs_low, visited;
+vector<int> s;
 int dfs_counter;
 int num_vertices;
-int root;
-int root_children;
 
 int UNVISITED = 1e9;
 
 int tarjan(int u) {
-  dfs_num[u] = dfs_low[u] = dfs_counter++;
-  s.push(u);
-  for(int i = 0; i < adjList[u].size(); i++) {
-    int front = adjList[u][i];
-    if(dfs_num[front] == UNVISITED) {
-      if(u == root) {
-        root_children++;
-      }
-      parent[front] = u;
-      tarjan(front);
-      dfs_low[u] = min(dfs_low[u], dfs_low[front]);
-    } else if(front != parent[u]) {
-      dfs_low[u] = min(dfs_low[u], dfs_num[front]);
+    dfs_num[u] = dfs_low[u] = dfs_counter++;
+    visited[u] = true;
+    s.push_back(u);
+    for(int i = 0; i < adjList[u].size(); i++) {
+        int front = adjList[u][i];
+        if(dfs_num[front] == UNVISITED)
+            tarjan(front);
+        if(visited[front])
+            dfs_low[u] = min(dfs_low[u], dfs_low[front]);
     }
-  }
-  if(dfs_num[u] == dfs_low[u]) {
-    cout << "----- Start -----" << endl;
-    while(true) {
-      int top = s.top();
-      s.pop();
-      cout << top << " ";
-      if(top == u) break;
+    if(dfs_num[u] == dfs_low[u]) {
+        cout << "----- Start -----" << endl;
+        while(true) {
+            int top = s.back();
+            s.pop_back();
+            visited[top] = false;
+            cout << top << " ";
+            if(top == u) break;
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
 }
 
 int main() {
-  scanf("%d", &num_vertices);
-  adjList.assign(num_vertices, vector<int>());
+    scanf("%d", &num_vertices);
+    adjList.assign(num_vertices, vector<int>());
 
-  int edge_count;
-  scanf("%d", &edge_count);
-  for(int i = 0; i < edge_count; i++) {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    adjList[a].push_back(b);
-    adjList[b].push_back(a);
-  }
-
-  dfs_num.assign(num_vertices, UNVISITED);
-  dfs_low.assign(num_vertices, 0);
-  parent.assign(num_vertices, 0);
-
-  for(int i = 0; i < num_vertices; i++) {
-    if(dfs_num[i] == UNVISITED) {
-      root = i;
-      root_children = 0;
-      tarjan(i);
+    int edge_count;
+    scanf("%d", &edge_count);
+    for (int i = 0; i < edge_count; i++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        adjList[a].push_back(b);
     }
-  }
 
-  return 0;
+    dfs_num.assign(num_vertices, UNVISITED);
+    dfs_low.assign(num_vertices, 0);
+    visited.assign(num_vertices, 0);
+
+    dfs_counter = 0;
+
+    for (int i = 0; i < num_vertices; i++)
+        if (dfs_num[i] == UNVISITED) tarjan(i);
+
+    return 0;
 }
